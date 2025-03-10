d61572bee7a2b40163ab81090490784c8ded0fba
developeranveshraman <official@anveshraman.rf.gd>
2025-03-10 22:56:34 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,73 @@
+var youtube = "https://www.youtube.com/@anveshraman/";
+var twitter = "https://www.twitter.com/anveshraman/";
+var password = "anveshraman.is-a.dev";
+var linkedin = "https://www.linkedin.com/in/anveshraman/";
+var instagram = "https://www.instagram.com/developeranveshraman/";
+var github = "https://github.com/developeranveshraman/";
+var email = 'mailto:support@anveshraman.rf.gd';
+
+whois = [
+  "<br>",
+  "Hey, I'm anveshraman!ðŸ‘‹",
+  "I'm a software engineer,game developer and web-developer who had created this site.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/@anveshraman' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/anveshraman' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/anveshraman' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/developeranveshraman' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/developeranveshraman' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+list = [
+  "<br>",
+  '<span class="command">whois</span>          Who is anveshraman?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">list</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">anveshraman.is-a.dev Not A Corporation. All knights reserved.</span>',
+"   ▄▄▄       ███▄    █ ██▒   █▓▓█████   ██████  ██░ ██  ██▀███   ▄▄▄       ███▄ ▄███▓ ▄▄▄       ███▄    █ ",
+"  ▒████▄     ██ ▀█   █▓██░   █▒▓█   ▀ ▒██    ▒ ▓██░ ██▒▓██ ▒ ██▒▒████▄    ▓██▒▀█▀ ██▒▒████▄     ██ ▀█   █ ",
+"  ▒██  ▀█▄  ▓██  ▀█ ██▒▓██  █▒░▒███   ░ ▓██▄   ▒██▀▀██░▓██ ░▄█ ▒▒██  ▀█▄  ▓██    ▓██░▒██  ▀█▄  ▓██  ▀█ ██▒",
+"  ░██▄▄▄▄██ ▓██▒  ▐▌██▒ ▒██ █░░▒▓█  ▄   ▒   ██▒░▓█ ░██ ▒██▀▀█▄  ░██▄▄▄▄██ ▒██    ▒██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒",
+"   ▓█   ▓██▒▒██░   ▓██░  ▒▀█░  ░▒████▒▒██████▒▒░▓█▒░██▓░██▓ ▒██▒ ▓█   ▓██▒▒██▒   ░██▒ ▓█   ▓██▒▒██░   ▓██░",
+"   ▒▒   ▓▒█░░ ▒░   ▒ ▒   ░ ▐░  ░░ ▒░ ░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ",
+"    ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ░░   ░ ░  ░░ ░▒  ░ ░ ▒ ░▒░ ░  ░▒ ░ ▒░  ▒   ▒▒ ░░  ░      ░  ▒   ▒▒ ░░ ░░   ░ ▒░",
+"    ░   ▒      ░   ░ ░     ░░     ░   ░  ░  ░   ░  ░░ ░  ░░   ░   ░   ▒   ░      ░     ░   ▒      ░   ░ ░ ",
+"        ░  ░         ░      ░     ░  ░      ░   ░  ░  ░   ░           ░  ░       ░         ░  ░         ░   ©2025",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'list'</span><span class=\"color2\">.</span>",
+];
